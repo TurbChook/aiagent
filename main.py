d21421f1baad08a5444a3d9d9d1e147b06060038
TurbChook <john.criollo.a@gmail.com>
2026-01-04 18:22:50 +0100

    Adding history of messages

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import os
+import argparse
+from google import genai
 from dotenv import load_dotenv
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 if api_key == None:
     raise RuntimeError("Gemini api-key not found.")
-
-from google import genai
-
 client = genai.Client(api_key=api_key)
+
+
 
 def main():
     print("Hello from aiagent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    # Now we can access `args.user_prompt`
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.5-flash', contents= messages
     )
-    print(response.text)
+
+    if response.usage_metadata == None:
+        raise RuntimeError("Property usage_metadata not found, failed API request.")
+    else:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
 
 if __name__ == "__main__":
     main()
