46f0af3fa646201ac5737d76a70a56c279fc17e9
TurbChook <john.criollo.a@gmail.com>
2026-01-07 21:37:25 +0100

    Modifying main function to let the agent choose which function to call

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,17 +5,16 @@ def write_file(working_directory, file_path, content):
         working_dir_abs = os.path.abspath(working_directory)
         file_abs = os.path.normpath(os.path.join(working_dir_abs, file_path))
         valid_target_dir = os.path.commonpath([working_dir_abs, file_abs]) == working_dir_abs
-        if valid_target_dir == False:
+        if not valid_target_dir:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         if os.path.isdir(file_abs):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
-        os.makedirs(file_path,exist_ok=True)
-        try:
-            with open(file_abs, "w") as f:
-                f.write(content)
-                return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
-        except Exception as e:
-            return f'Error: {e}'
+        dir_name = os.path.dirname(file_abs)
+        if dir_name:
+            os.makedirs(dir_name, exist_ok=True)
+        with open(file_abs, "w") as f:
+            f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f'Error: {e}'
 schema_write_file = types.FunctionDeclaration(
--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from google import genai
 from dotenv import load_dotenv
 from google.genai import types
 from prompts import system_prompt
-from functions import *
-from call_functions import *
+from call_function import available_functions, call_function
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -22,24 +21,34 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
     # Now we can access `args.user_prompt`
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', contents= messages, config = types.GenerateContentConfig(tools = [available_functions], system_instruction = system_prompt),
-    )
-
-    if response.usage_metadata == None:
+    generate_content(client,messages,args.verbose)
+def generate_content(client, messages,verbose):
+    response =  client.models.generate_content(
+        model='gemini-2.5-flash', contents= messages, config = types.GenerateContentConfig(tools = [available_functions], system_instruction = system_prompt),)
+    if not response.usage_metadata:
         raise RuntimeError("Property usage_metadata not found, failed API request.")
-    else:
-        if(args.verbose == True):
-            print(f"User prompt: {args.user_prompt}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not response.function_calls:
         print("Response:")
-        if response.function_calls is None:
-            print(response.text)
-        else:
-            for function_call in response.function_calls:
-                print(f"Calling function: {function_call.name}({function_call.args})")
-
+        print(response.text)
+        return
+    function_responses = []
+    for function_call in response.function_calls:
+        function_call_result = call_function(function_call,verbose)
+        if not function_call_result.parts:
+            raise Exception("No parts returned from function call")
+        first_part = function_call_result.parts[0]
+        if not first_part.function_response:
+            raise Exception("Function response is None")
+        if first_part.function_response.response is None:
+            raise Exception("Function response body is None")
+        if verbose:
+            print(f"-> {first_part.function_response.response['result']}")
+        function_responses.append(first_part)
 if __name__ == "__main__":
     main()
